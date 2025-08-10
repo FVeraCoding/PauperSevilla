40452586c6ad57261a037a7a3208864186f1019e
Fvera <fveracoding@gmail.com>
2025-08-10 09:07:57 +0200

    Añadido proyecto de Angular para el frontend

---
+++ Frontend/PauperSevilla/src/app/src/app/components/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+
+}
